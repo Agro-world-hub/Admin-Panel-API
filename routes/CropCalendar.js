57847743648172103d9e41dec0747297a1bd02d3
Chalana Prabhashwara <120502017+GMchalana@users.noreply.github.com>
2025-02-02 21:23:34 +0530

    Update CropCalendar.js

--- routes/CropCalendar.js
+++ routes/CropCalendar.js
@@ -8,20 +8,9 @@ const path = require("path");
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-        cb(
-            null,
-            file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-        );
-    },
-});
+
 
 const uploadfile = multer({
-    // storage: storage,
     fileFilter: function (req, file, callback) {
         var ext = path.extname(file.originalname);
         if (ext !== ".xlsx" && ext !== ".xls") {
