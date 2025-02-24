05622cdf1d333b71a49a3b177249984615821043
bimangunarathne <bimangunarathne@outlook.com>
2025-02-24 18:45:52 +0530

    refactor getAllCustomers to modularize SQL query construction and improve readability

--- dao/Dash-dao.js
+++ dao/Dash-dao.js
@@ -4,39 +4,95 @@ const {
     collectionofficer,
     marketPlace,
     dash,
-  } = require("../startup/database");
+} = require("../startup/database");
 
-  const Joi = require("joi");
+const Joi = require("joi");
 
-exports.getAllCustomers = () => {
+
+const getHouseDetails = () => `
+    CASE 
+        WHEN c.buildingType = 'House' THEN h.houseNo 
+        ELSE NULL 
+    END AS houseHouseNo,
+    CASE 
+        WHEN c.buildingType = 'House' THEN h.streetName 
+        ELSE NULL 
+    END AS houseStreetName,
+    CASE 
+        WHEN c.buildingType = 'House' THEN h.city 
+        ELSE NULL 
+    END AS houseCity
+`;
+
+// Function to get apartment details if the customer lives in an apartment
+const getApartmentDetails = () => `
+    CASE 
+        WHEN c.buildingType = 'Apartment' THEN a.buildingNo 
+        ELSE NULL 
+    END AS apartmentBuildingNo,
+    CASE 
+        WHEN c.buildingType = 'Apartment' THEN a.buildingName 
+        ELSE NULL 
+    END AS apartmentBuildingName,
+    CASE 
+        WHEN c.buildingType = 'Apartment' THEN a.unitNo 
+        ELSE NULL 
+    END AS apartmentUnitNo,
+    CASE 
+        WHEN c.buildingType = 'Apartment' THEN a.floorNo 
+        ELSE NULL 
+    END AS apartmentFloorNo,
+    CASE 
+        WHEN c.buildingType = 'Apartment' THEN a.houseNo 
+        ELSE NULL 
+    END AS apartmentHouseNo,
+    CASE 
+        WHEN c.buildingType = 'Apartment' THEN a.streetName 
+        ELSE NULL 
+    END AS apartmentStreetName,
+    CASE 
+        WHEN c.buildingType = 'Apartment' THEN a.city 
+        ELSE NULL 
+    END AS apartmentCity
+`;
+
+// Function to construct the SQL query
+const getAllCustomersQuery = () => `
+    SELECT 
+        c.id, 
+        c.cusId, 
+        c.title, 
+        c.firstName, 
+        c.lastName, 
+        c.phoneNumber, 
+        c.email, 
+        c.buildingType, 
+        s.empId AS salesAgentEmpId,  
+        s.firstName AS salesAgentFirstName,  
+        s.lastName AS salesAgentLastName,  
+        c.created_at,
+        ${getHouseDetails()},
+        ${getApartmentDetails()}
+    FROM customer c
+    LEFT JOIN salesagent s ON c.salesAgent = s.id  
+    LEFT JOIN house h ON c.id = h.customerId AND c.buildingType = 'House'  
+    LEFT JOIN apartment a ON c.id = a.customerId AND c.buildingType = 'Apartment'  
+    ORDER BY c.created_at DESC
+`;
+
+// Function to execute the query and fetch customer data
+const getAllCustomers = () => {
     return new Promise((resolve, reject) => {
-      const sql = `
-        SELECT 
-          c.id, 
-          c.cusId, 
-          c.title, 
-          c.firstName, 
-          c.lastName, 
-          c.phoneNumber, 
-          c.email, 
-          c.buildingType, 
-          s.empId AS salesAgentEmpId,  -- Get empId from salesagent
-          s.firstName AS salesAgentFirstName,  -- Get firstName from salesagent
-          s.lastName AS salesAgentLastName,  -- Get lastName from salesagent
-          c.created_at 
-        FROM customer c
-        LEFT JOIN salesagent s ON c.salesAgent = s.id  -- Correctly join salesagent table
-        ORDER BY c.created_at DESC`;
-  
-      dash.query(sql, (err, results) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(results);
-      });
+        dash.query(getAllCustomersQuery(), (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(results);
+        });
     });
-  };
+};
 
-  
-  
-  
+module.exports = { getAllCustomers };
+
+
+
