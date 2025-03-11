34fa56889cf49c7575f3c627db9ec172665ac3c9
bimangunarathne <bimangunarathne@outlook.com>
2025-03-11 14:27:10 +0530

    feat: refactor getOfficerTargetDao and getSelectedOfficerTarget to simplify parameters and improve error handling

--- dao/Target-dao.js
+++ dao/Target-dao.js
@@ -1,6 +1,70 @@
 const {plantcare, collectionofficer, marketPlace, dash} = require('../startup/database')
 
-exports.getOfficerTargetDao = (userId, status, search) => {
+// exports.getOfficerTargetDao = (userId, status, search) => {
+//     return new Promise((resolve, reject) => {
+//         let sql =
+//             `SELECT 
+//             ODT.id, 
+//             ODT.dailyTargetId, 
+//             ODT.varietyId, 
+//             CV.varietyNameEnglish, 
+//             CG.cropNameEnglish, 
+//             ODT.target, 
+//             ODT.grade, 
+//             ODT.complete, 
+//             DT.toDate, 
+//             DT.toTime, 
+//             CO.empId,
+//             CASE 
+//                 WHEN ODT.target > ODT.complete THEN 'Pending'
+//                 WHEN ODT.target < ODT.complete THEN 'Exceeded'
+//                 WHEN ODT.target = ODT.complete THEN 'Completed'
+//             END AS status,
+//             CASE 
+//                 WHEN ODT.complete > ODT.target THEN 0.00
+//                 ELSE ODT.target - ODT.complete
+//             END AS remaining
+//         FROM dailytarget DT 
+//         JOIN officerdailytarget ODT ON ODT.dailyTargetId = DT.id 
+//         JOIN plant_care.cropvariety CV ON ODT.varietyId = CV.id 
+//         JOIN plant_care.cropgroup CG ON CV.cropGroupId = CG.id
+//         JOIN collectionofficer CO ON ODT.officerId = CO.id
+//         WHERE CO.id = ?`;
+
+//         const params = [userId];
+
+//         // Add the status condition if it is provided
+//         if (status) {
+//             sql += ` AND (
+//                 CASE 
+//                     WHEN ODT.target > ODT.complete THEN 'Pending'
+//                     WHEN ODT.target < ODT.complete THEN 'Exceeded'
+//                     WHEN ODT.target = ODT.complete THEN 'Completed'
+//                 END
+//             ) = ?`;
+//             params.push(status);
+//         }
+
+//         if (search) {
+//             sql += ` AND (CV.varietyNameEnglish LIKE ? OR CG.cropNameEnglish LIKE ?)`;
+//             params.push(`%${search}%`, `%${search}%`);
+//         }
+
+//         console.log("Executing SQL:", sql);
+//         console.log("With Parameters:", params);
+
+//         collectionofficer.query(sql, params, (err, results) => {
+//             if (err) {
+//                 console.error("Error executing query:", err);
+//                 return reject(err);
+//             }
+//             console.log("Query Results:", results);
+//             resolve(results);
+//         });
+//     });
+// };
+
+exports.getOfficerTargetDao = (userId) => {
     return new Promise((resolve, reject) => {
         let sql =
             `SELECT 
@@ -27,33 +91,21 @@ exports.getOfficerTargetDao = (userId, status, search) => {
         FROM dailytarget DT 
         JOIN officerdailytarget ODT ON ODT.dailyTargetId = DT.id 
         JOIN plant_care.cropvariety CV ON ODT.varietyId = CV.id 
-        JOIN plant_care.CropGroup CG ON CV.cropGroupId = CG.id
+        JOIN plant_care.cropgroup CG ON CV.cropGroupId = CG.id
         JOIN collectionofficer CO ON ODT.officerId = CO.id
         WHERE CO.id = ?`;
 
         const params = [userId];
 
-        // Add the status condition if it is provided
-        if (status) {
-            sql += ` AND (
-                CASE 
-                    WHEN ODT.target > ODT.complete THEN 'Pending'
-                    WHEN ODT.target < ODT.complete THEN 'Exceeded'
-                    WHEN ODT.target = ODT.complete THEN 'Completed'
-                END
-            ) = ?`;
-            params.push(status);
-        }
-
-        if (search) {
-            sql += ` AND (CV.varietyNameEnglish LIKE ? OR CG.cropNameEnglish LIKE ?)`;
-            params.push(`%${search}%`, `%${search}%`);
-        }
+        console.log("Executing SQL:", sql);
+        console.log("With Parameters:", params);
 
         collectionofficer.query(sql, params, (err, results) => {
             if (err) {
+                console.error("Error executing query:", err);
                 return reject(err);
             }
+            console.log("Query Results:", results);
             resolve(results);
         });
     });
