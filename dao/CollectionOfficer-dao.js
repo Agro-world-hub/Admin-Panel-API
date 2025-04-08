5ca9c1ac7c4afa1e4138c2da90998f74f2c4ae03
GMchalana <gmchalanaprabhashwara@gmail.com>
2025-04-08 12:48:22 +0530

    Add downloadCollectionReport endpoint and implement Excel file generation for collection data

--- dao/CollectionOfficer-dao.js
+++ dao/CollectionOfficer-dao.js
@@ -1983,3 +1983,95 @@ exports.getCollectionReport = (page, limit, centerId, startDate, endDate, search
   });
 };
 
+
+
+
+
+
+
+exports.downloadCollectionReport = (centerId, startDate, endDate, search) => {
+  return new Promise((resolve, reject) => {
+  
+
+    const params = [];
+    const countParams = [];
+    const totalParams = [];
+
+    
+    let whereClause = "WHERE c.id = 1";
+
+    if (centerId) {
+      whereClause += " AND co.centerId = ?";
+      params.push(centerId);
+      countParams.push(centerId);
+      totalParams.push(centerId);
+    }
+
+    if (startDate && endDate) {
+      whereClause += " AND DATE(rfp.createdAt) BETWEEN ? AND ?";
+      params.push(startDate, endDate);
+      countParams.push(startDate, endDate);
+      totalParams.push(startDate, endDate);
+    } else if (startDate) {
+      whereClause += " AND DATE(rfp.createdAt) >= ?";
+      params.push(startDate);
+      countParams.push(startDate);
+      totalParams.push(startDate);
+    } else if (endDate) {
+      whereClause += " AND DATE(rfp.createdAt) <= ?";
+      params.push(endDate);
+      countParams.push(endDate);
+      totalParams.push(endDate);
+    }
+
+    if (search) {
+      whereClause += `
+        AND (
+          cc.regCode LIKE ? OR 
+          cc.centerName LIKE ? OR 
+          cg.cropNameEnglish LIKE ? OR
+          cv.varietyNameEnglish LIKE ?
+        )
+      `;
+      const searchPattern = `%${search}%`;
+      params.push(searchPattern, searchPattern, searchPattern, searchPattern);
+      countParams.push(searchPattern, searchPattern, searchPattern, searchPattern);
+      totalParams.push(searchPattern, searchPattern, searchPattern, searchPattern);
+    }
+
+   
+    let dataSql = `
+      SELECT 
+        fpc.id AS id,
+        cc.regCode AS regCode,
+        cc.centerName AS centerName,
+        cg.cropNameEnglish AS cropGroupName,
+        cv.varietyNameEnglish AS varietyName,
+        fpc.gradeAquan AS gradeAquan,
+        fpc.gradeBquan AS gradeBquan,
+        fpc.gradeCquan AS gradeCquan,
+        SUM(IFNULL(fpc.gradeAquan, 0) + IFNULL(fpc.gradeBquan, 0) + IFNULL(fpc.gradeCquan, 0)) AS amount,
+        fpc.createdAt AS createdAt
+      FROM 
+        farmerpaymentscrops fpc
+      JOIN registeredfarmerpayments rfp ON fpc.registerFarmerId = rfp.id
+      JOIN collectionofficer co ON rfp.collectionOfficerId = co.id
+      JOIN plant_care.users us ON rfp.userId = us.id
+      JOIN collectioncenter cc ON co.centerId = cc.id
+      JOIN company c ON co.companyId = c.id
+      JOIN plant_care.cropvariety cv ON fpc.cropId = cv.id
+      JOIN plant_care.cropgroup cg ON cv.cropGroupId = cg.id
+      ${whereClause}
+      GROUP BY fpc.id
+    `;
+
+    console.log('Executing Count Query...');
+
+    collectionofficer.query(dataSql, params, (err, results) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(results);
+    });
+  });
+};
