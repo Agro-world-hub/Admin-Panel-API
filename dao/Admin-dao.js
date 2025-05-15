2785b7d79e739e62f2075b01c0f75c300160bc9a
GMchalana <gmchalanaprabhashwara@gmail.com>
2025-05-15 09:44:13 +0530

    Add invoice number to payment slip report query

--- dao/Admin-dao.js
+++ dao/Admin-dao.js
@@ -2254,6 +2254,7 @@ exports.getPaymentSlipReport = (
           u.NICnumber,
           co.firstNameEnglish AS officerFirstName,
           co.lastNameEnglish AS officerLastName,
+          rp.invNo,
           rp.createdAt
       FROM 
           registeredfarmerpayments rp
