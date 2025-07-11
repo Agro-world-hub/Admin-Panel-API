9e3ce4339153b2ad7c2c180e045ea49d837b07ec
Pasan-FDo <pasanfernando.work@gmail.com>
2025-07-11 22:47:15 +0530

    update test route response message

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ app.use(BASE_PATH + '/api/distribution', DistributionRoutes)
 app.use("/uploads", express.static("uploads"));
 
 app.get(BASE_PATH + "/test", (req, res) => {
-  res.send("Test route is working 7/2!");
+  res.send("Test route is working 7/11!");
   console.log("test route is working");
 });
 
