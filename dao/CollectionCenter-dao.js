aaa7f18274043adabd30369ec6b3cb7c0b2c7089
Pasan-FDo <pasanfernando.work@gmail.com>
2025-08-04 15:00:19 +0530

    fix reply complain part

--- dao/CollectionCenter-dao.js
+++ dao/CollectionCenter-dao.js
@@ -481,7 +481,24 @@ exports.getComplainById = (id) => {
 exports.getCenterComplainById = (id) => {
   return new Promise((resolve, reject) => {
     const sql = ` 
-    SELECT oc.id, oc.refNo, oc.createdAt, oc.language, oc.complain, oc.complainCategory, oc.reply, cof.firstNameEnglish AS firstName, cof.lastNameEnglish AS lastName, cof.phoneCode01, cof.phoneNumber01,  cc.categoryEnglish AS complainCategory, cof.empId AS empId, cof.jobRole AS jobRole
+    SELECT 
+      oc.id, 
+      oc.refNo, 
+      oc.createdAt, 
+      oc.language, 
+      oc.complain, 
+      oc.complainCategory, 
+      oc.reply, 
+      cof.firstNameEnglish AS firstName, 
+      cof.lastNameEnglish AS lastName, 
+      cof.phoneCode01, 
+      cof.phoneNumber01,  
+      cc.categoryEnglish AS complainCategory, 
+      cof.empId AS empId, 
+      cof.jobRole AS jobRole,
+      CONCAT (cof.firstNameEnglish, ' ', cof.lastNameEnglish) AS officerName,
+      CONCAT (cof.firstNameSinhala, ' ', cof.lastNameSinhala) AS officerNameSinhala,
+      CONCAT (cof.firstNameTamil, ' ', cof.lastNameTamil) AS officerNameTamil
     FROM officercomplains oc
     LEFT JOIN collectionofficer cof ON oc.officerId = cof.id
     LEFT JOIN agro_world_admin.complaincategory cc ON oc.complainCategory = cc.id
